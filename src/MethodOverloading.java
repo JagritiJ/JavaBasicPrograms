35250ed944cc8f4b6d0907489c68df162f3434c3
Jagriti Jain <jagritij@vmware.com>
2018-12-02 19:40:51 +0530

    Method Overloading

---
+++ src/MethodOverloading.java
@@ -0,0 +1,18 @@
+public class MethodOverloading {
+
+
+    static int add(int x, int y){
+
+        return x+y;
+    }
+
+    static int add(int x, int y, int z){
+
+        return x+y+z;
+    }
+
+    public static void main(String args[]){
+       System.out.println( add(4,6));
+       System.out.println(add(2,1,8));
+    }
+}
